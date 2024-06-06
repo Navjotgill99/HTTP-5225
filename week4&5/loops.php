d9745651e8fa223e334456c3cd291b681982efeb
Navjotgill99 <navjotkaurgill41@gmail.com>
2024-06-05 22:46:13 -0400

    Modified file

--- week4&5/loops.php
+++ week4&5/loops.php
@@ -46,15 +46,37 @@
                 </div>';
               }
             }
+
+            if (!empty($users)) {
+              foreach ($users as $user) {
+                  echo
+                  '<div class="col-md-3">
+                <div class="card" style="">
+                <div class="card-body">
+                    <h5 class="card-title">' . $user['name'] . '</h5>
+                    <p class="card-text">Username - ' . $user['username'] . '</p>
+                    <p class="card-text">Email - ' . $user['email'] . '</p>
+                    <p class="card-text">Phone - ' . $user['phone'] . '</p>
+                    <p class="card-text">Website - ' . $user['website'] . '</p>
+                    <h5 class="card-title">Address</h5>
+                    <p class="card-text">Street - ' . $user['address']['street'] . '</p>
+                    <p class="card-text">Suite - ' . $user['address']['suite'] . '</p>
+                    <p class="card-text">City - ' . $user['address']['city'] . '</p>
+                    <p class="card-text">Zipcode - ' . $user['address']['zipcode'] . '</p>
+                    <p class="card-text">Geo -lat: ' . $user['address']['geo']['lat'] . '</p>
+                    <p class="card-text">Geo -lng: ' . $user['address']['geo']['lng'] . '</p>
+                    <h5 class="card-title">Company</h5>
+                    <p class="card-text">Name - ' . $user['company']['name'] . '</p>
+                    <p class="card-text">CatchPhrase - ' . $user['company']['catchPhrase'] . '</p>
+                    <p class="card-text">Bs - ' . $user['company']['bs'] . '</p>
+                  </div>
+              </div>
+              </div>';
+              }
+          }
           ?>
         </div>
       </div>
     </div>
-
-    <?php
-    
-    
-
-  ?>
 </body>
 </html>
